307f8e9a1c57ea473d81df656b79616756510ab5
Flamerace <datadragonrace@gmail.com>
2026-07-25 15:57:36 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -286,6 +286,7 @@ dependencies {
     implementation(libs.androidx.paging.common)
 
     // Media & Files
+    implementation(libs.androidx.media3.common)
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.media3.session)
