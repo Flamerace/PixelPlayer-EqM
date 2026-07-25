94aa6b4e5e5110683c4af6f5837bd6815630b1bd
Flamerace <datadragonrace@gmail.com>
2026-07-25 13:16:15 +0530

    Update DualPlayerEngine.kt

--- app/src/main/java/com/theveloper/pixelplay/data/service/player/DualPlayerEngine.kt
+++ app/src/main/java/com/theveloper/pixelplay/data/service/player/DualPlayerEngine.kt
@@ -223,7 +223,8 @@ class DualPlayerEngine @Inject constructor(
     private val jellyfinStreamProxy: com.theveloper.pixelplay.data.jellyfin.JellyfinStreamProxy,
     private val gdriveStreamProxy: com.theveloper.pixelplay.data.gdrive.GDriveStreamProxy,
     private val telegramCacheManager: com.theveloper.pixelplay.data.telegram.TelegramCacheManager,
-    private val connectivityStateHolder: com.theveloper.pixelplay.presentation.viewmodel.ConnectivityStateHolder
+    private val connectivityStateHolder: com.theveloper.pixelplay.presentation.viewmodel.ConnectivityStateHolder, 
+    private val dynamicBassManager: com.theveloper.pixelplay.data. equalizer.DynamicBassManager
 ) {
     private companion object {
         private const val AUDIO_OFFLOAD_STALL_FALLBACK_MS = 4_000L
