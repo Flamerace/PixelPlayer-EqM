155fe2d635fa5964a68bea924555d0edf81abffe
Flamerace <datadragonrace@gmail.com>
2026-07-25 17:11:17 +0530

    Update EqualizerScreen.kt

--- app/src/main/java/com/theveloper/pixelplay/presentation/screens/EqualizerScreen.kt
+++ app/src/main/java/com/theveloper/pixelplay/presentation/screens/EqualizerScreen.kt
@@ -111,6 +111,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
