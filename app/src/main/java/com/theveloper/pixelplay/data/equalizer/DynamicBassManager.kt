24c92fe456c905c8fe00b3a0c1edc1cac76db9ed
Kevan Niroshana <datadragonrace@gmail.com>
2026-07-28 17:02:16 +0530

    Update DynamicBassManager.kt

--- app/src/main/java/com/theveloper/pixelplay/data/equalizer/DynamicBassManager.kt
+++ app/src/main/java/com/theveloper/pixelplay/data/equalizer/DynamicBassManager.kt
@@ -104,6 +104,7 @@ class DynamicBassManager @Inject constructor() {
 package com.theveloper.pixelplay.data.equalizer
 
 import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
